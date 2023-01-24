840a4a9c04a4dcf91ee7fd4af0f7d85ab3506aac
elouan50 <elouan.colybes@gmail.com>
2023-01-24 17:24:14 +0100

    add

---
+++ src/main/java/org/centrale/note/Classe1.java
@@ -0,0 +1,35 @@
+package org.centrale.note;
+
+public class Classe1 {
+    private int var1;
+    private String var2;
+
+    /**
+     * @return int return the var1
+     */
+    public int getVar1() {
+        return var1;
+    }
+
+    /**
+     * @param var1 the var1 to set
+     */
+    public void setVar1(int var1) {
+        this.var1 = var1;
+    }
+
+    /**
+     * @return String return the var2
+     */
+    public String getVar2() {
+        return var2;
+    }
+
+    /**
+     * @param var2 the var2 to set
+     */
+    public void setVar2(String var2) {
+        this.var2 = var2;
+    }
+
+}
