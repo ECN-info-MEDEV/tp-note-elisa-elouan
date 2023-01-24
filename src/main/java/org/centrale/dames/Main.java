c77ec4ff37c9cbbc4c7d192cf2cbefa578bafe69
elouan50 <elouan.colybes@gmail.com>
2023-01-24 16:33:41 +0100

    add first

---
+++ src/main/java/org/centrale/dames/Main.java
@@ -0,0 +1,11 @@
+package org.centrale.dames;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        sc = new Scanner(System.in);
+        System.out.println("Hello World!")
+    }
+}
