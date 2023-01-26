40f73f6433fc9f688ba3ca8e722a063bb74e452a
Elisa <elisa@andrianarison.com>
2023-01-26 09:18:02 +0100

    jeu joueur pb init

---
+++ src/main/java/org/centrale/note/Verif.java
@@ -0,0 +1,12 @@
+package org.centrale.note;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Verif {
+    private List<PionVerif> verif;
+
+    public Verif(){
+        List<PionVerif> verif = new ArrayList<PionVerif>();
+    }
+}
